7ecd008426d7003f9452a2629ad4e04a64d68677
kunalja <kunaljain@utexas.edu>
2022-07-17 17:14:39 -0400

    day 2

---
+++ src/bin/02a.rs
@@ -0,0 +1,26 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+fn main() -> std::io::Result<()> {
+    let INPUT_PATH = "inputs/02.in";
+    let input_f = File::open(INPUT_PATH)?;
+    let reader = BufReader::new(input_f);
+    
+    let mut depth = 0;
+    let mut horizontal_pos = 0;
+    for l in reader.lines() {
+        let l = l?;
+        let args: Vec<&str> = l.split_whitespace().collect();
+        match args[0] {
+            "up" => depth -= args[1].parse().unwrap_or(0),
+            "down" => depth += args[1].parse().unwrap_or(0),
+            "forward" => horizontal_pos += args[1].parse().unwrap_or(0),
+            _ => println!("other")
+        };
+    }
+    
+    println!("Result: {} x {} = {}", depth, horizontal_pos, depth * horizontal_pos);
+
+    Ok(())
+}
